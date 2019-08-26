8a82ef864c0961da229bcec3e45c52b9b843560c
AvixSoft Games <AvixSoft486@gmail.com>
2019-08-26 13:51:58 -0700

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -398,8 +398,8 @@ if (BABYLON.Engine.isSupported()) {
         ground.checkCollisions = true;
         ground.material = new BABYLON.StandardMaterial("txtGround", scene);
         ground.material.diffuseTexture = new BABYLON.Texture("gfx/Textures/floor_stone_2.png", scene);
-        ground.material.diffuseTexture.uScale = 1.0;
-        ground.material.diffuseTexture.vScale = 1.0;
+        ground.material.diffuseTexture.uScale = 10.0;
+        ground.material.diffuseTexture.vScale = 10.0;
 
         genCubes(scene);
         renderAnimatedMonsters(scene);
